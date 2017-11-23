7d99cdc5b7ef4272fb0cc5f24d596cb6a5aaaa9d
Sergey <krockssb@gmail.com>
2017-11-23 17:29:23 +0200

    tested database

--- app/src/main/java/com/bablo/goni/gonibablo/ui/MainActivity.kt
+++ app/src/main/java/com/bablo/goni/gonibablo/ui/MainActivity.kt
@@ -6,6 +6,8 @@ import com.bablo.goni.gonibablo.R
 import com.bablo.goni.gonibablo.arch.room.AppDataBase
 import com.bablo.goni.gonibablo.arch.room.SomeDao
 import com.bablo.goni.gonibablo.di.MyApp
+import com.bablo.goni.gonibablo.model.Session
+import com.bablo.goni.gonibablo.model.getSampleSession
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -19,6 +21,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         MyApp.graph.inject(this)
+        val incomingSession = getSampleSession()
+        dao.saveSession(incomingSession)
+
+        val temSes = dao.getAllSessions()
+        Timber.d(temSes.toString())
     }
 
     override fun onResume() {
